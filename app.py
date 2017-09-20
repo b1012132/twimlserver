ef47fbe36955dd16335fc9032c13b245adc5140d
瀬川 貴雅 <segawa.takamasa@jsd.jp>
2017-09-20 16:41:11 +0900

    Init Commit

---
+++ app.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+from twilio.twiml.voice_response import VoiceResponse
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	resp = VoiceResponse()
+	resp.say(u"こんにちは。何かお困りですか。", language="ja-JP", voice="alice")
+	return str(resp)
+
+if __name__ == "__main__":
+    app.run()
