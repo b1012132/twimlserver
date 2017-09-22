1d61bf89c893b05aa3ff3900433fb989ef2b4ce5
Takamasa Segawa <b1012132@users.noreply.github.com>
2017-09-22 11:53:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from twilio.twiml.voice_response import VoiceResponse
+import requests
 
 app = Flask(__name__)
 
@@ -8,7 +9,18 @@ app = Flask(__name__)
 def hello():
 	resp = VoiceResponse()
 	resp.say(u"こんにちは。何かお困りですか。", language="ja-JP", voice="alice")
+	resp.Record(timeout=10, maxLength=30, finishOnKey="1234567890*#", recordingStatusCallback="https://twimlserver.herokuapp.com/record")
+	
 	return str(resp)
+
+@app.route("/record", methods=['POST'])
+def record():
+	url = request.form['RecordingUrl']
+	r = requests.get(url)
+	
+	wavFile = open(,'w')#File path here
+	wavFile.write(r.content)
+	wavFile.close()
 
 if __name__ == "__main__":
     app.run()
