43262bca0e91ecd63c43d4c6230e23121e38dd20
瀬川 貴雅 <segawa.takamasa@jsd.jp>
2017-09-22 14:52:24 +0900

    record file transcript in app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 from flask import Flask
 from twilio.twiml.voice_response import VoiceResponse
 import requests
+import json
+import os
 
 app = Flask(__name__)
 
@@ -20,17 +22,26 @@ def record():
 	r = requests.get(wavUrl)
 	
 	watsonUrl = 'https://stream.watson-j.jp/speech-to-text/api/v1/sessions?model=ja-JP_BroadbandModel&word_confidence=true'
-	username = 
-	password =
+	username = os.environ["STT_USERNAME"]
+	password = os.environ["STT_PASSWORD"]
 	headers = {'Content-Type': 'audio/wav'}
 	audio = r.content
 	r2 = requests.post(url, data=audio, headers=headers, auth=(username, password))
 	
 	res = json.loads(r2.text)
 	if(len(res['results']) == 0):
-		say ="sorry"			
+		say = u"ごめんなさい、聞き取れませんでした。"
 	else:
-		for result in res['results']:			transcript =									
-
+		for result in res['results']:
+			for alternative in result['alternative']
+				transcript = alternative['transcript']
+				confidence = alternative['confidence']
+		say = transcript.encode('utf-8')
+		
+	resp = VoiceResponse()
+	resp.say(say, language="ja-JP", voice="alice")
+	
+	return str(resp)
+	
 if __name__ == "__main__":
     app.run()
