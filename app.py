49730a211a4641d4903dc9b41921fba7d32ecb77
Takamasa Segawa <b1012132@users.noreply.github.com>
2017-09-25 11:00:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -22,30 +22,28 @@ def record():
 	#wavUrl = request.form['RecordingUrl']
 	r = requests.get("http://dl3.jvckenwood.com/pro/avc/product/pa-d_message/ai_arigatougozaimasita.wav")
 	
-	watsonUrl = 'https://stream.watson-j.jp/speech-to-text/api/v1/sessions?model=ja-JP_BroadbandModel&word_confidence=true'
+	watsonUrl = 'https://stream.watson-j.jp/speech-to-text/api/v1/recognize?continuous=true&model=ja-JP_BroadbandModel&word_confidence=true'
 	username = os.environ["STT_USERNAME"]
 	password = os.environ["STT_PASSWORD"]
 	headers = {'Content-Type': 'audio/wav'}
 	audio = r.content
-	print r.status_code#
-	print audio
-	#r2 = requests.post(watsonUrl, data=audio, headers=headers, auth=(username, password))
+	r2 = requests.post(watsonUrl, data=audio, headers=headers, auth=(username, password))
 	
-	#res = json.loads(r2.text)
-	#print res
-	#if(len(res['results']) == 0):
-	#	say = u"ごめんなさい、聞き取れませんでした。"
-	#else:
-	#	for result in res['results']:
-	#		for alternative in result['alternative']:
-	#			transcript = alternative['transcript']
-	#			confidence = alternative['confidence']
-	#	say = transcript.encode('utf-8')
-	#	
-	#resp = VoiceResponse()
-	#resp.say(say, language="ja-JP", voice="alice")
+	res = json.loads(r2.text)
+
+	if(len(res['results']) == 0):
+		say = u"ごめんなさい、聞き取れませんでした。"
+	else:
+		for result in res['results']:
+			for alternative in result['alternative']:
+				transcript = alternative['transcript']
+				confidence = alternative['confidence']
+		say = transcript.encode('utf-8')
+		
+	resp = VoiceResponse()
+	resp.say(say, language="ja-JP", voice="alice")
 	
-	#return str(resp)
+	return str(resp)
 	
 if __name__ == "__main__":
     app.run()
