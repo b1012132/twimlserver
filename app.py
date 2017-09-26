08002929cc59fb943385c0f5710f909ce813111f
瀬川 貴雅 <segawa.takamasa@jsd.jp>
2017-09-26 13:40:12 +0900

    fix reply url

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def record():
 	authToken = os.environ["AUTH_TOKEN"]
 	client = Client(accountSid, authToken)
 	
-	call = client.calls(callSid).update(url="https://twimlserver.herokuapp.com/reply/"+say, method="GET")
+	call = client.calls(callSid).update(url="https://twimlserver.herokuapp.com/reply?say="+say, method="GET")
 	
 	return
 	
